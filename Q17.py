daaf7aa38225d15316b89c0dae99d7a4aa39c3a8
StorM <123388343+Purvesh271@users.noreply.github.com>
2023-08-15 09:40:50 +0530

    Delete Q17.py

--- Q17.py
+++
@@ -1,8 +0,0 @@
-n=input("Enter the phone number:")
-if len(n)==12:
-    s=n.split("-")
-    if len(s[0])==3 and len(s[1])==3 and len(s[2])==4:
-        if s[0].isdigit() and s[1].isdigit() and s[2].isdigit():
-            print("phone number is valid")
-else:
-    print("phone number is invalid")
