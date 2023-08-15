c327ab9deaf815690669784e0cf9574b8854d0c0
StorM <123388343+Purvesh271@users.noreply.github.com>
2023-08-15 09:59:58 +0530

    Add files via upload

---
+++ dict of lang.py
@@ -0,0 +1,11 @@
+favlang ={}
+f1 = input("Enter your fav language a:\n")
+f2 = input("Enter your fav language b:\n")
+f3 = input("Enter your fav language c:\n")
+f4 = input("Enter your fav language d:\n")
+favlang['a']= f1
+favlang['b']= f2
+favlang['c']= f3
+favlang['d']= f4
+
+print(favlang)
---
+++ dict.py
@@ -0,0 +1,8 @@
+mydict = {
+    "pankha" :"fan",
+    "dabba":"box",
+    "bartan":"utensiles"
+}
+print("options are :",mydict.keys())
+a= input("Enter the hindi word:\n")
+print("The meaning of your hindi word is:",mydict[a])
