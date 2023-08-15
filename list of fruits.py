e2479ee595b7df37110aa0e434df3434a9830b9c
StorM <123388343+Purvesh271@users.noreply.github.com>
2023-08-15 09:59:34 +0530

    Add files via upload

---
+++ list of fruits.py
@@ -0,0 +1,10 @@
+l1 = input("Enter the name of fruit1:")
+l2 = input("Enter the name of fruit2:")
+l3 = input("Enter the name of fruit3:")
+l4 = input("Enter the name of fruit4:")
+l5 = input("Enter the name of fruit5:")
+l6 = input("Enter the name of fruit6:")
+l7 = input("Enter the name of fruit7:")
+
+lst = [l1,l2,l3,l4,l5,l6,l7]
+print(lst)
---
+++ list of marks.py
@@ -0,0 +1,10 @@
+a= input("enter the marks of student 1:")
+b= input("enter the marks of student 2:")
+c= input("enter the marks of student 3:")
+d= input("enter the marks of student 4:")
+e= input("enter the marks of student 5:")
+f= input("enter the marks of student 6:")
+g= input("enter the marks of student 7:")
+lst = [a,b,c,d,e,f,g]
+lst.sort()
+print(lst)
